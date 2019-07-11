b1bca48f74c03c7d48dbf326778a6f3c91fb0c54
Davide Bettio <davide.bettio@ispirata.com>
2019-07-11 18:27:54 +0200

    Output a JSON object having videoUrl and audioUrl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <QtCore/QCoreApplication>
 #include <QtCore/QDebug>
 
+#include <QtCore/QJsonDocument>
+#include <QtCore/QJsonObject>
+
 #include <iostream>
 #include <cstdlib>
 
@@ -18,13 +21,25 @@ int main(int argc, char **argv)
     YTVideo *video = new YTVideo(app.arguments().at(1));
 
     QObject::connect(video, &YTVideo::gotStreamUrl, [] (const QString &videoUrl, const QString &audioUrl) {
-        std::cout << "VIDEO: " << videoUrl.toUtf8().constData() << std::endl;
-        std::cout << "AUDIO: " << audioUrl.toUtf8().constData() << std::endl;
+        //std::cout << "VIDEO: " << videoUrl.toUtf8().constData() << std::endl;
+        //std::cout << "AUDIO: " << audioUrl.toUtf8().constData() << std::endl;
+
+        QJsonObject o;
+        if (!videoUrl.isEmpty()) {
+            o.insert("videoUrl", videoUrl);
+        }
+        if (!audioUrl.isEmpty()) {
+            o.insert("audioUrl", audioUrl);
+        }
+        QJsonDocument doc;
+        doc.setObject(o);
+        std::cout << doc.toJson(QJsonDocument::Compact).constData() << std::endl;
+
         QCoreApplication::quit();
     });
 
     QObject::connect(video, &YTVideo::errorStreamUrl, [] (const QString &message) {
-        std::cout << "ERROR: " << message.toUtf8().constData() << std::endl;
+        std::cerr << "ERROR: " << message.toUtf8().constData() << std::endl;
         QCoreApplication::exit(EXIT_FAILURE);
     });
 
